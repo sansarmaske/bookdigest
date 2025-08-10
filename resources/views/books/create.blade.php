eee76091de5d7610f8ddea25cbbbed06d2c9b4d7
Sansar Maske <sansarmaske@gmail.com>
2025-08-09 20:56:28 -0400

    fix styling issues for button on books/create

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -58,7 +58,7 @@
                         </div>
 
                         <div class="flex items-center justify-end space-x-4">
-                            <a href="{{ route('books.index') }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                            <a href="{{ route('books.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                 Cancel
                             </a>
                             <x-primary-button>
